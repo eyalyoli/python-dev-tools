0e298762be587aaae9c4b9bdf678a80b55da6faf
Eyal Yoli Abs <eyalyoli@gmail.com>
2022-04-24 09:39:46 +0300

    bug free version
    bug free version

--- req-auto-cleaner.py
+++ req-auto-cleaner.py
@@ -2,9 +2,13 @@ import argparse
 import os
 import re
 from pathlib import Path
-from logging import Logger, INFO
+import logging 
+import sys
 
-logger = Logger(level=INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+DELETION_COMMENT = ' # req-auto-cleaner: commented for deletion'
+TENTATIVE_COMMENT = ' # req-auto-cleaner: check package usage manually'
 
 # -> {req1: [line1, line2], req2: ...}
 def get_requirements(path: str) -> dict:
@@ -15,7 +19,7 @@ def get_requirements(path: str) -> dict:
         line_index = 0
         for line in file.readlines():
             line_index += 1
-            req_name = re.match('^[\w-]+', line)
+            req_name = re.match('^[\w.-]+', line)
             if not req_name: # skip empty lines
                 continue
             req_name = req_name.group(0).lower()
@@ -25,10 +29,13 @@ def get_requirements(path: str) -> dict:
             if pip_command:
                 add_prev_line = True
             else:
-                result[req_name] = [line_index]
+                if req_name not in result:
+                    result[req_name] = [line_index]
+                else:
+                    result[req_name].append(line_index)
                 if add_prev_line:
                     result[req_name].append(line_index-1)
-        
+                    add_prev_line = False
     return result             
 
 
@@ -49,48 +56,72 @@ def get_imported_deps(project_path: str) -> list:
                 else:
                     # no more import lines -> skip
                     break
-    
     return result
 
-def delete_lines_from_file(path: str, lines_to_delete: list):
+def modify_line(line: str, prefix: str = None, suffix: str = None):
+    res = line
+    if prefix:
+        res = prefix + res
+    if suffix:
+        if res.endswith('\n'):
+            res = res.replace('\n', suffix + '\n')
+        else:
+            res = res + suffix
+    return res
+
+def delete_lines_from_file(
+    path: str, 
+    lines_to_delete: list, 
+    tentative_lines: list, 
+    delete_permanently=False
+):
     lines = None
     with open(path, 'r') as f:
         lines = f.readlines()
     with open(path, 'w') as f:
         for idx, line in enumerate(lines):
-            if idx not in lines_to_delete:
-                f.write(line)
+            if (idx + 1) in lines_to_delete:
+                if not delete_permanently:
+                    f.write(modify_line(line, prefix='# ', suffix=DELETION_COMMENT))
+            elif (idx + 1) in tentative_lines:
+                f.write(modify_line(line, suffix=TENTATIVE_COMMENT))
+            else:
+                f.write(line) 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Requirements auto-cleaner')
     parser.add_argument('project', type=str, help="Project's root path")
     parser.add_argument(
-        '-r', type=str, help="Requirements file alternative path relative to project's root", default='requirements.txt')
-    # parser.add_argument('--dry-run', dest='dryrun', action='store_true', help="Don't write the results to a file")
-    # parser.set_defaults(dryrun=False)
+        '-r', 
+        type=str, 
+        help="Requirements file alternative path relative to project's root", 
+        default='requirements.txt'
+    )
 
     args = parser.parse_args()
     req_file_path = os.path.join(args.project, args.r)
 
     reqs = get_requirements(req_file_path)
-    logger.debug('Requirements mapping:\n', reqs)
+    logging.debug('Requirements mapping:\n' + str(reqs))
+    logging.info(f'Detected {len(reqs)} packages in requirement file')
     deps = sorted(get_imported_deps(args.project), key=len)
-    logger.debug('Imports mapping:\n', deps)
-    logger.debug('-----------')
+    logging.debug('Imports mapping:\n' + str(deps))
+    logging.info(f'Analyzed {len(deps)} imported packages in code')
+    logging.debug('-----------')
 
     # check lines to delete
     delete_lines = []
     tentative_lines = []
     for req, lines in reqs.items():
         if req not in deps:
-            if '-' in req:
-                tentative_lines.append((req, lines))
+            if '-' in req: # handle multi-word package names
+                tentative_lines += lines
             else:
                 delete_lines += lines
         else:
             deps.remove(req)
 
-    logger.info('Deleteing the following lines:', delete_lines)
-    delete_lines_from_file(req_file_path, delete_lines)
-    logger.info('You are clean!!')
-    print('Needs a manual recheck:', tentative_lines)
+    logging.info('Deleteing the following lines: ' + str(delete_lines))
+    delete_lines_from_file(req_file_path, delete_lines, tentative_lines)
+    print('You are clean!!')
+    print('Lines need a manual check: ' + str(tentative_lines))
